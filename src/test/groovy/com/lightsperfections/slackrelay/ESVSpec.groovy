6fe1174b84ba6fab7e3174b0f18730a345cfa895
Jon Hughes <jon.hughes@bodybuilding.com>
2015-11-25 15:43:36 -0800

    Trying to get RESTClient working... it don't

--- src/test/groovy/com/lightsperfections/slackrelay/ESVSpec.groovy
+++ src/test/groovy/com/lightsperfections/slackrelay/ESVSpec.groovy
@@ -1,6 +1,9 @@
 package com.lightsperfections.slackrelay;
 
 import spock.lang.Specification;
+import groovyx.net.http.HTTPBuilder;
+import static groovyx.net.http.ContentType.*;
+import static groovyx.net.http.Method.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,8 +12,15 @@ import spock.lang.Specification;
  * Time: 2:31 PM
  */
 public class ESVSpec extends Specification {
-    def "let's try this!"() {
-        expect:
-        Math.max(1, 2) == 3
+    def "should return 200"() {
+        setup:
+        def primerEndpoint = new RESTClient( 'http://localhost:8080/' )
+        when:
+        def resp = primerEndpoint.get([ path: 'esv', query : [ input: 'texty' ]])
+        then:
+        with(resp) {
+            status == 200
+            contentType == "application/json"
+        }
     }
 }
