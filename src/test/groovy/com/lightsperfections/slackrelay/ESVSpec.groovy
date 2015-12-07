aadf4e58a628ba0fda1cfb6588c7e9e0f51cd72e
Jon Hughes <jon.hughes@bodybuilding.com>
2015-12-07 08:43:55 -0800

    Testing with embedded server is working now

--- src/test/groovy/com/lightsperfections/slackrelay/ESVSpec.groovy
+++ src/test/groovy/com/lightsperfections/slackrelay/ESVSpec.groovy
@@ -1,6 +1,14 @@
 package com.lightsperfections.slackrelay
 
-import groovyx.net.http.RESTClient;
+import groovyx.net.http.RESTClient
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.IntegrationTest
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration;
 import spock.lang.Specification;
 
 /**
@@ -9,10 +17,19 @@ import spock.lang.Specification;
  * Date: 11/18/15
  * Time: 2:31 PM
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = SpringApplicationContextLoader, classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port=9000")
 public class ESVSpec extends Specification {
+
+    @Value("\${server.port}")
+    private int port;
+
+    @Test
     def "should return 200"() {
         setup:
-        def primerEndpoint = new RESTClient( 'http://localhost:8080/' )
+        def primerEndpoint = new RESTClient( "http://localhost:$port/" )
         when:
         def resp = primerEndpoint.get([ path: 'esv', query : [ input: 'texty' ]])
         then:
