56431be36fa2faa4069c89cec411ba2879d3e86e
Jon Hughes <hughes.jon@gmail.com>
2015-12-11 15:03:41 -0800

    getting there with POST test

--- src/test/groovy/com/lightsperfections/slackrelay/ESVSpec.groovy
+++ src/test/groovy/com/lightsperfections/slackrelay/ESVSpec.groovy
@@ -1,6 +1,8 @@
 package com.lightsperfections.slackrelay
 
 import groovyx.net.http.RESTClient
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.ContentType.URLENC
 import groovyx.net.http.ContentType.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,12 +23,12 @@ import spock.lang.Specification;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = SpringApplicationContextLoader, classes = Application.class)
 @WebAppConfiguration
-@IntegrationTest("server.port=9000")
+@IntegrationTest("server.port=8080")
 public class ESVSpec extends Specification {
 
     @Value("\${server.port}")
     private int port;
-
+/*
     @Test
     def "GET is an invalid request method"() {
         setup:
@@ -39,27 +41,26 @@ public class ESVSpec extends Specification {
             status == 405
         }
     }
-
+*/
     @Test
     def "POST with all params returns success"() {
         setup:
         def client = new RESTClient( "http://localhost:$port/" )
         client.handler.failure = client.handler.success
-        def paramMap = [:]
-        paramMap["token"] = "token";
-        paramMap["teamId"] = "teamId";
-        paramMap["teamDomain"] = "teamDomain";
-        paramMap["channelId"] = "channelId";
-        paramMap["channelName"] = "channelName";
-        paramMap["userId"] = "userId";
-        paramMap["userName"] = "userName";
-        paramMap["command"] = "command";
-        paramMap["text"] = "text";
+        def paramMap = [token: 'token',
+                        teamId: 'teamId',
+                        channelId: 'channelId',
+                        channelName: 'channelName',
+                        userId: 'userId',
+                        userName: 'userName',
+                        command: 'command',
+                        text: 'text']
         when:
         def resp = client.post(
                 path: 'esv',
-                body: paramMap,
-                requestContentType: "application/x-www-form-urlencoded" )
+                requestContentType: URLENC,
+                body: paramMap
+        )
         then:
         with(resp) {
             status == 200
