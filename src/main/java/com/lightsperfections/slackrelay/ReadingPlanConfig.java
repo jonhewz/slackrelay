eb921153632d404bb28335b0062419ed50d5c4f1
Jon Hughes <hughes.jon@gmail.com>
2016-01-29 15:25:20 -0800

    Added Horner's original reading plan.

--- src/main/java/com/lightsperfections/slackrelay/ReadingPlanConfig.java
+++ src/main/java/com/lightsperfections/slackrelay/ReadingPlanConfig.java
@@ -22,6 +22,23 @@ public class ReadingPlanConfig {
 
         return readingPlan;
     }
+
+    @Bean(name = "horner")
+    public ReadingPlan getHorner() {
+        ReadingPlan readingPlan = new ReadingPlan("horner");
+        readingPlan.addTrack(new Book[]{Book.MT, Book.MR, Book.LU, Book.JOH});
+        readingPlan.addTrack(new Book[]{Book.GE, Book.EX, Book.LE, Book.NU, Book.DE});
+        readingPlan.addTrack(new Book[]{Book.RO, Book.CO1, Book.CO2, Book.GA, Book.EPH, Book.PHP, Book.COL, Book.HEB});
+        readingPlan.addTrack(new Book[]{Book.TH1, Book.TH2, Book.TI1, Book.TI2, Book.TIT, Book.PHM, Book.JAS, Book.PE1, Book.PE2, Book.JO1, Book.JO2, Book.JO3, Book.JUDE, Book.RE});
+        readingPlan.addTrack(new Book[]{Book.JOB, Book.EC, Book.SO});
+        readingPlan.addTrack(new Book[]{Book.PS});
+        readingPlan.addTrack(new Book[]{Book.PR});
+        readingPlan.addTrack(new Book[]{Book.JOS, Book.JUD, Book.RU, Book.SA1, Book.SA2, Book.KI1, Book.KI2, Book.CH1, Book.CH2, Book.EZR, Book.NE, Book.ES});
+        readingPlan.addTrack(new Book[]{Book.ISA, Book.JER, Book.LA, Book.EZE, Book.DA, Book.HO, Book.JOE, Book.AM, Book.OB, Book.JON, Book.MIC, Book.NA, Book.HAB, Book.ZEC, Book.HAG, Book.ZEC, Book.MAL});
+        readingPlan.addTrack(new Book[]{Book.AC});
+
+        return readingPlan;
+    }
 }
 
 /*
